cc55bc0189709e01b6291e61848de88b8694e2aa
greisonsantos <greisonsantos03@gmail.com>
2019-09-01 10:26:32 -0300

    initials files

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+console.tron.log('bem vindo teste');
+const Welcome = () => (
+  <View>
+    <Text> bem vindo teste</Text>
+  </View>
+);
+
+export default Welcome;
