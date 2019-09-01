ae186e91ac3241b89a3f68bf6a5e23ac40d66c10
greisonsantos <greisonsantos03@gmail.com>
2019-09-01 11:29:26 -0300

    Welcome Style

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, StatusBar} from 'react-native';
 
 import styles from './styles';
 
 console.tron.log('bem vindo teste ');
 const Welcome = () => (
   <View style={styles.container}>
-    <Text> bem vindo teste</Text>
+    <StatusBar barStyle="light-content" backgroundColor="black" />
+
+    <Text style={styles.title}> Bem vindo !!</Text>
+    <Text style={styles.text}>
+      Para continuar Precisamos que você informe seu User do GItHub
+    </Text>
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu Usuário"
+        underlineColorAndroid="transparent"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Prosseguir </Text>
+      </TouchableOpacity>
+    </View>
   </View>
 );
 
